684a58f5a6ab4b743e62451d9a63187a1f9c3913
Esteban <Estebankramer@hotmail.com>
2019-04-05 09:55:36 -0300

    EventDrivenMoleculeDynamics

---
+++ EventDrivenMoleculeDynamics/src/Engine.java
@@ -0,0 +1,14 @@
+
+public class Engine {
+    private final static double INFINITE = Double.MAX_VALUE;
+
+
+    public double timeUntilCrashWithParticle(Particle p1, Particle p2) {
+        Point deltaR = new Point(p2.getLocation().getX() - p1.getLocation().getX(), p2.getLocation().getY() - p1.getLocation().getY());
+        Point deltaV = new Point(p)
+    }
+
+    public double timeUntilCrashWithWall(Particle p, int l) {
+
+    }
+}
