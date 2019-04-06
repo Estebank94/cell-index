56ddbcf8d00f71d46197199119eb3eb56567e7a5
Esteban <Estebankramer@hotmail.com>
2019-04-06 16:03:18 -0300

    Main setup

---
+++ EventDrivenMoleculeDynamics/src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * Created by estebankramer on 06/04/2019.
+ */
+public class Main {
+
+    public static void main( String[] args ){
+        Engine engine = new Engine(300);
+        engine.start(300, "/Users/estebankramer1/Desktop/results");
+    }
+}
