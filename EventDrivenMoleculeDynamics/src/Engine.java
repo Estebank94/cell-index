8f56346c66038abf7acf63fb53157714b2ea7dee
Esteban <Estebankramer@hotmail.com>
2019-04-06 16:03:12 -0300

    Print particles

--- EventDrivenMoleculeDynamics/src/Engine.java
+++ EventDrivenMoleculeDynamics/src/Engine.java
@@ -1,4 +1,7 @@
 import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.*;
 
 public class Engine {
@@ -19,8 +22,8 @@ public class Engine {
 
     private double maxSmallVelocity = 0.1;
 
-    private int collisionCount;
-    private double time;
+    private int collisionCount = 0;
+    private double time = 0;
 
     public Engine(int amountOfParticles) {
         this.particles = new HashSet<>();
@@ -95,19 +98,7 @@ public class Engine {
      * STEP 5
      *
      * **/
-    public void evolveCrashedParticles(Set<Particle> crashedParticles){
-        Iterator<Particle> pIterator = crashedParticles.iterator();
-        if(crashedParticles.size() == 1) {
-            Particle p = pIterator.next();
-            if(crashedAgainstVerticalWall(p)) {
-                p.setVx(-p.getVx());
-            } else {
-                p.setVy(-p.getVy());
-            }
-        } else {
-            Particle p1 = pIterator.next();
-            Particle p2 = pIterator.next();
-
+    public void evolveCrashedParticles(Particle p1, Particle p2){
             double sigma = calculateSigma(p1, p2);
             Point deltaR = calculateDeltaR(p1, p2);
             Point deltaV = calculateDeltaV(p1, p2);
@@ -121,6 +112,14 @@ public class Engine {
             p1.setVy(p1.getVy() + Jy / p1.getMass());
             p2.setVx(p2.getVx() - Jx / p2.getMass());
             p2.setVy(p2.getVy() - Jy / p2.getMass());
+    }
+
+
+    public void evolveCrashedParticles(Particle p){
+        if(crashedAgainstVerticalWall(p)) {
+            p.setVx(-p.getVx());
+        } else {
+            p.setVy(-p.getVy());
         }
     }
 
@@ -208,30 +207,83 @@ public class Engine {
         }
     }
 
-    public void startEngine (int simulationTime, String path){
+    public void start(int simulationTime, String path) {
+        System.out.println("Starting simulation...");
+        System.out.println("Number of Particles: " + particles.size());
         double particleCrashTc;
         double tc = Double.POSITIVE_INFINITY;
-        Particle crashed1;
-        Particle crashed2;
-
-        for(Particle p1 : particles){
-            particleCrashTc = timeUntilCrashWithWall(p1);
-            if(particleCrashTc < tc) {
-                tc = particleCrashTc;
-                crashed1 = p1;
-                crashed2 = null;
-            }
-            for(Particle p2 : particles){
-                if(!p1.equals(p2)){
-                    particleCrashTc = timeUntilCrashWithParticle(p1, p2);
-                    if(particleCrashTc < tc ) {
-                        tc = particleCrashTc;
-                        crashed2 = p2;
+        Particle crashed1 = null;
+        Particle crashed2 = null;
+        while( time < simulationTime ) {
+            for(Particle p1 : particles){
+                particleCrashTc = timeUntilCrashWithWall(p1);
+                if(particleCrashTc < tc) {
+                    tc = particleCrashTc;
+                    crashed1 = p1;
+                    crashed2 = null;
+                }
+                for(Particle p2 : particles){
+                    if(!p1.equals(p2)){
+                        particleCrashTc = timeUntilCrashWithParticle(p1, p2);
+                        if(particleCrashTc < tc ) {
+                            tc = particleCrashTc;
+                            crashed1 = p1;
+                            crashed2 = p2;
+                        }
                     }
                 }
+
             }
 
+            updatePositions(tc);
+            if(crashed2 != null) {
+                evolveCrashedParticles(crashed1, crashed2);
+            } else {
+                evolveCrashedParticles(crashed1);
+            }
+
+            time += tc;
+
+            //System.out.println("tc: " + tc + " - " + pi.toString());
+//            System.out.println(particles.size() + 2);
+            System.out.println("Collision Count: " + collisionCount++ + " time: " + time);
+//            for (Particle p : particles){
+//                System.out.println(p.getPosition().getX() + "\t" + p.getPosition().getY() + "\t" + p.getVx() + "\t" + p.getVy() + "\t" + p.getRadius() + "\t" + tc);
+//            }
+
+            String toWrite = generateFileString();
+            Engine.writeToFile(toWrite,collisionCount,path);
+
+            // Print two particles for Ovito animation
+//            System.out.println(0 + "\t" + 0 + "\t" + 0 + "\t" + 0 + "\t" + 0.001 + "\t" + 0);
+//            System.out.println(L + "\t" + L + "\t" + 0 + "\t" + 0 + "\t" + 0.001 + "\t" + 0);
         }
+
+    }
+
+    public static void writeToFile(String data, int index, String path){
+        try {
+            Files.write(Paths.get(path  + "/result" + index + ".txt"), data.getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public String generateFileString(){
+        StringBuilder builder = new StringBuilder()
+                .append("//ID\t X\t Y\t Radius\t R\t G\t B\t\r\n");
+        for(Particle current: particles){
+            builder.append(current.getId())
+                    .append(" ")
+                    .append(current.getPosition().getX())
+                    .append(" ")
+                    .append(current.getPosition().getY())
+                    .append(" ")
+                    .append(current.getRadius())
+                    .append("\r\n");
+        }
+        return builder.toString();
     }
 
 
