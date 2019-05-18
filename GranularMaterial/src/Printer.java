8e5cc3d7d2c68c7164cb366ac231379fae0c4355
Esteban <Estebankramer@hotmail.com>
2019-05-18 20:06:36 -0300

    Print improvements

--- GranularMaterial/src/Printer.java
+++ GranularMaterial/src/Printer.java
@@ -1,7 +1,6 @@
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -11,95 +10,82 @@ public class Printer {
     double L;
     double W;
     double D;
-    static int i = 0;
-    public Printer(String outPath,double L, double W,double D) {
+
+    public Printer(String path, double L, double W,double D) {
 
         this.L = L;
         this.W = W;
         this.D = D;
+
         try {
-            bw = new BufferedWriter(new FileWriter(outPath  + i++ + ".txt", true));
+            bw = new BufferedWriter(new FileWriter(path + ".txt", true));
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public void close() {
-        if (bw != null) try {
-            bw.flush();
-            bw.close();
-        } catch (IOException ioe2) {
-            // just ignore it
+    private int addBorders(Set<Particle> borders) {
+        double radius = 0.01;
+        double step = radius/2;
+        int count =- 1000000;
+
+        for (double i = -radius; i < W + radius; i += step){
+            if(i > W - (W/2 - D/2) || i < W/2 - D/2){
+                borders.add(new Particle(count++,i,-radius,0,0,radius,0));
+            }
         }
+
+        for(double i =- radius ; i < L+radius; i += step){
+            borders.add(new Particle(count++,-radius,i,0,0,radius,0));
+            borders.add(new Particle(count++,W+radius,i,0,0,radius,0));
+        }
+
+        return borders.size();
     }
 
+    private String generateFileString(Set<Particle> particles) {
+        Set<Particle> borders = new HashSet<>();
 
-    private String generateFileString(Set<Particle> allParticles) {
-        Set<Particle> limits = new HashSet<>();
-//        limits.addAll(
-//                Arrays.asList(new Particle(-1, 0, 0, 0, 0, 0, 0),
-//                        new Particle(-2, W, 0, 0, 0, 0, 0),
-//                        new Particle(-3, 0, L, 0, 0, 0, 0),
-//                        new Particle(-4, W, L, 0, 0, 0, 0)));
-        addBorders(limits);
+        addBorders(borders);
+
         StringBuilder builder = new StringBuilder()
-                .append(allParticles.size() + limits.size())
-                .append("\r\n")
-                .append("//ID\t X\t Y\t Radius\t vx\t vy\t FN\t \r\n");
+                .append(particles.size() + borders.size() + "\r\n")
+                .append("//ID\t X\t Y\t Radius\t VX\t VY\t FN\t \r\n");
+        
+        appendParticles(particles, builder);
+        appendParticles(borders, builder);
 
-        appendParticles(allParticles, builder);
-        appendParticles(limits, builder);
         return builder.toString();
     }
 
-    private int addBorders(Set<Particle> limits) {
-        double delta= 1/200.0;
-        int count=1;
-        double radius = 0.01;
-        for(double i =-radius ; i< L+radius;i+=delta){
-            limits.add(new Particle(-4-count++,-radius,i,0,0,radius,0));
-            limits.add(new Particle(-4-count++,W+radius,i,0,0,radius,0));
-        }
-        for (double j= -radius; j< W+radius;j+=delta){
-            if(j < W/2 - D/2 || j > W - (W/2 - D/2))
-                limits.add(new Particle(-4-count++,j,-radius,0,0,radius,0));
-        }
-        return count;
-    }
-
-    private void appendParticles(Set<Particle> allParticles, StringBuilder builder) {
-        for (Particle current : allParticles) {
-            double vx = current.getSpeed().x;
-            double vy = current.getSpeed().y;
-            double fn = current.getTotalFn()/Math.PI*2*current.getRadius();
-            builder.append(current.getId())
+    private void appendParticles(Set<Particle> particles, StringBuilder builder) {
+        for (Particle p : particles) {
+            builder.append(p.getId())
                     .append(" ")
-                    .append(current.getPosition().x)
+                    .append(p.getPosition().x)
                     .append(" ")
-                    .append(current.getPosition().y)
+                    .append(p.getPosition().y)
                     .append(" ")
-                    .append(current.getRadius())
+                    .append(p.getRadius())
                     .append(" ")
-                    .append(new Double(vx).floatValue())
+                    .append(new Double(p.getSpeed().x).floatValue())
                     .append(" ")
-                    .append(new Double(vy).floatValue())
+                    .append(new Double(p.getSpeed().y).floatValue())
                     .append(" ")
-                    .append(new Double(fn).floatValue())
+                    .append(new Double(p.getTotalFn() / Math.PI * 2 * p.getRadius()).floatValue())
                     .append("\r\n");
-
-
         }
     }
 
-
-    public void appendToFile(Set<Particle> allParticles){
-        appendToFile(generateFileString(allParticles));
+    public void appendToFile(Set<Particle> particles){
+        appendToFile(generateFileString(particles));
     }
-    public void appendToFile( String data) {
+
+    public void appendToFile(String data) {
         try {
             bw.write(data);
-        } catch (IOException ioe) {
-            ioe.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
